2496502a213127b75c50a5a7b7646b4364b4fbc9
rohit-rayavarapu <rohitchowdary78@gmail.com>
2023-02-01 08:52:50 +0530

    first commit for the basic counter

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+
+let counter = ref(5);
+</script>
+
+
+<template>
+  <main>
+    <div>
+      <h4>The Current count is</h4>
+      <h1>{{ counter }}</h1>
+      <button @click="counter--">-</button>
+      <button @click="counter++">+</button>
+    </div>
+  </main>
+</template>
+
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
